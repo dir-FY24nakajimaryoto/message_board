17afbba29259672b69fae1fbf9348758acb612b1
dir-FY24nakajimaryoto <ryoto.nakajima.m632@dirbato.co.jp>
2024-04-22 16:30:37 +0900

    バリデーションを追加

--- message_board/src/models/Message.java
+++ message_board/src/models/Message.java
@@ -46,40 +46,40 @@ public class Message {
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContent() {
         return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 
     public Timestamp getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(Timestamp created_at) {
-        this.created_at = created_at;
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public Timestamp getUpdated_at() {
         return updated_at;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public void setUpdated_at(Timestamp updated_at) {
